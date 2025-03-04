ffc2efd7c03bca795f6f64f6963d0b49bcd43c58
Zakaal <mochammadzakaro24@gmail.com>
2025-03-04 15:03:10 +0700

    update pertemuan 3

---
+++ 03-pengenalan-nextjs/src/my-react/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () =>{
+  return (
+    <div>
+      <h1>Selamat Datang di Website Kaka!</h1>
+      <p>Ini adalah halaman utama.</p>
+    </div>
+  );
+}
+
+export default HomePage;
